5f5a2efcbedc86ce305077c68d70f6faeb878a27
supermarine1377 <supermarine@outlook.jp>
2024-12-15 15:23:04 +0900

    Refactor Logger

--- internal/monitorer/logger/logger.go
+++ internal/monitorer/logger/logger.go
@@ -11,31 +11,28 @@ import (
 )
 
 type Logger struct {
-	files []io.Writer
+	file io.Writer
 }
 
 func New(createLogFile bool) (*Logger, error) {
-	files := make([]io.Writer, 0, 2)
-	files = append(files, os.Stdout)
+	var w io.Writer = os.Stdout
 
 	if createLogFile {
 		logFile, err := os.Create(fileName(context.Background()))
 		if err != nil {
 			return nil, err
 		}
-		files = append(files, logFile)
+		w = io.MultiWriter(w, logFile)
 	}
 
-	return &Logger{files: files}, nil
+	return &Logger{file: w}, nil
 }
 
 func (l *Logger) Logln(ctx context.Context, r *models.Response) {
 	t := timectx.NowStr(ctx)
 	s := t + " " + r.Status
 	b := []byte(s + "\n")
-	for _, f := range l.files {
-		_, _ = f.Write(b)
-	}
+	_, _ = l.file.Write(b)
 }
 
 func (l *Logger) Error(ctx context.Context, err error) {
@@ -43,9 +40,7 @@ func (l *Logger) Error(ctx context.Context, err error) {
 	s := t + " " + err.Error()
 	fmt.Fprintln(os.Stderr, s)
 	b := []byte(err.Error())
-	for _, f := range l.files {
-		_, _ = f.Write([]byte(b))
-	}
+	_, _ = l.file.Write(b)
 }
 
 func fileName(ctx context.Context) string {
